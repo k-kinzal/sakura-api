d4c3a11dfa78548406595c04d04936bbe8e94147
k-kinzal <kinzal@logn.in>
2014-10-04 13:09:15 +0900

    create mongo service

--- public/index.php
+++ public/index.php
@@ -3,6 +3,21 @@
 $loader = require('../vendor/autoload.php');
 // application
 $app = new \Slim\Slim();
+// service
+$app->container->singleton('mongo', function () {
+    // connect to Compose assuming your MONGOHQ_URL environment
+    // variable contains the connection string
+		$connection_url = getenv("MONGOHQ_URL");
+     // create the mongo connection object
+    $m = new MongoClient($connection_url);
+    // extract the DB name from the connection path
+    $url = parse_url($connection_url);
+    $db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
+    // use the database we connected to
+    $db = $m->selectDB($db_name);
+
+    return $db;
+});
 // config
 $app->config(array(
 	'debug' => true,
@@ -10,6 +25,8 @@ $app->config(array(
 ));
 // routing
 $app->get('/:name', function($name) use($app) {
+	$mongo = $app->mongo;
+	var_dump($mongo);
 	$app->render('json.php', array('name' => $name));
 });
 $app->post('/:name', function($name) use($app) {
